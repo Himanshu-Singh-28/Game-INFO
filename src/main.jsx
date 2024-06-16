c586417714c08aa196a2e08595def93ef7062564
Himanshu Kumar Singh <himanshusingh1234h93@gmail.com>
2024-06-16 22:16:10 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
-export const API_KEY = import.meta.env.VITE_API_KEY;
+export const API_KEY = "247040a5f9944bb29d72ac3598c4c296";
 
 export const RapidHeaders = [
   {
-    "X-RapidAPI-Key": import.meta.VITE_API_KEY_RAPID_1,
+    "X-RapidAPI-Key": "acd04e6207mshe9783c824d176adp1b0284jsn3cccb2ff75c0",
     "X-RapidAPI-Host": "steam-api7.p.rapidapi.com",
   },
   {
-    "X-RapidAPI-Key": import.meta.VITE_API_KEY_RAPID_2,
+    "X-RapidAPI-Key": "0f4cc9d942mshb36fd39151b0382p10dfb1jsnd2b6ab366892",
     "X-RapidAPI-Host": "steam-api7.p.rapidapi.com",
   },
 ];
